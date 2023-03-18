34b83e37052d6f2d8a3be85252b05bfef1622cbb
kalbek <access2kaleb@gmail.com>
2023-03-18 18:10:30 +0300

    add small gray horizontal line under speakers description

--- script/script.js
+++ script/script.js
@@ -8,7 +8,7 @@ function main() {
         'Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School',
       subText:
         'Benkler studies commons-based peer production, and published his seminal book, The Wealth of Networks in 2006',
-      imageUrl: '../images/home/speaker-01.png',
+      imageUrl: './images/home/speaker-01.png',
     },
     {
       id: 2,
@@ -16,7 +16,7 @@ function main() {
       mainText: 'Director of Art Centre Nabi and a board member of CC Korea',
       subText:
         'As the main venue for new media art production in Korea, Nabi promotes cross-disciplinary collaboration and understanding among science technology, humanities, and the arts.',
-      imageUrl: '../images/home/speaker-03.png',
+      imageUrl: './images/home/speaker-03.png',
     },
     {
       id: 3,
@@ -24,7 +24,7 @@ function main() {
       mainText: 'Director of Art Centre Nabi and a board member of CC Korea',
       subText:
         'As the main venue for new media art production in Korea, Nabi promotes cross-disciplinary collaboration and understanding among science technology, humanities, and the arts.',
-      imageUrl: '../images/home/speaker-03.png',
+      imageUrl: './images/home/speaker-03.png',
     },
     {
       id: 4,
@@ -32,7 +32,7 @@ function main() {
       mainText: '',
       subText:
         'Kilnam Chon helped bring the internet to Asia and is an outspoken advocate for the open web and digital com-mons. In 2012. he was inducted into the inaugural class of the Internet Society’s (ISOC) Internet Hall of Fame',
-      imageUrl: '../images/home/speaker-02.png',
+      imageUrl: './images/home/speaker-02.png',
     },
     {
       id: 5,
@@ -40,14 +40,14 @@ function main() {
       mainText: 'President of Young Pirates of Europe',
       subText:
         'European ingetration, political democracy and participation of youth through online as her major condern, Reda’s report outlining potential changes to EU copyright law was approved by the Parliament in July',
-      imageUrl: '../images/home/speaker-04.png',
+      imageUrl: './images/home/speaker-04.png',
     },
     {
       id: 6,
       title: 'Ryan Merkley',
       mainText: 'CEO of Creativve Commons, ex COO of the Mozilla Foundation',
       subText: '',
-      imageUrl: '../images/home/speaker-06.png',
+      imageUrl: './images/home/speaker-06.png',
     },
   ];
 
@@ -63,6 +63,7 @@ function main() {
             <div class='text-content'>
               <div class='title'>${speaker.title}</div>
               <div class='main-text'>${speaker.mainText}</div>
+              <div class='gray-bar'></div>
             <div class='sub-text'>${speaker.subText}</div>
           </div>
         </div>
